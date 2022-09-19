b866c0d420c154c8e218c85e54f3528987398721
Jamie Stankiewiz <jamie.stankiewiz@berkeley.edu>
2022-09-18 19:34:02 -0600

    using mavutil instead?

---
+++ NUC_connection_test.py
@@ -0,0 +1,12 @@
+from dronekit import connect
+from pymavlink import mavutil
+
+ip = '/dev/ttyUSB0'
+# through dronekit.connect
+cube_orange = connect(ip=ip, baud=115200, use_native=True)
+# figure out where to put a message
+print(cube_orange.gps_0)
+
+# through mavutil
+cube_orange_connection = mavutil.mavlink_connection(ip)
+cube_orange_connection.mav.send('test') # something like that
