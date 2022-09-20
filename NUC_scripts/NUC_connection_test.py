43cf536e60613f4705299bb05d9be79cde128755
Jamie Stankiewiz <jamie.stankiewiz@berkeley.edu>
2022-09-20 13:22:17 -0600

    prelim script for testing

--- NUC_scripts/NUC_connection_test.py
+++ NUC_scripts/NUC_connection_test.py
@@ -1,12 +1,20 @@
 from dronekit import connect
-from pymavlink import mavutil
+from pymavlink import mavutil, MAVLink_message
 
 ip = '/dev/ttyUSB0'
+
+
 # through dronekit.connect
-cube_orange = connect(ip=ip, baud=115200, use_native=True)
+cube_orange = connect(ip=ip, baud=115200, use_native=True) # returns a Vehicle object
 # figure out where to put a message
 print(cube_orange.gps_0)
 
+@cube_orange.on_message('HEARTBEAT')
+def message(name: str, msg: MAVLink_message):
+    print(msg)
+
+
+
 # through mavutil
 cube_orange_connection = mavutil.mavlink_connection(ip)
-cube_orange_connection.mav.send('test')
+cube_orange_connection.mav.send('test') # something like that
