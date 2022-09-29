864d358bf7de3069eead22417d4a7f2daa75083b
Jamie Stankiewiz <jamie.stankiewiz@berkeley.edu>
2022-09-29 11:39:21 -0600

    still troubleshooting a way to connect to mission planner. currently both script and MP cannot connect to com6 simultaneously

--- NUC_scripts/NUC_connection_test.py
+++ NUC_scripts/NUC_connection_test.py
@@ -1,4 +1,5 @@
 from pymavlink import mavutil # use mavlink2
+
 '''
 Some useful info:
     system ID : Mission Planner
@@ -6,27 +7,34 @@ Some useful info:
     message ID :  (note: MAVLink 2 messages have an ID > 255)
     payload : actual data
     target system ID : 255 (= Mission Planner)
-    target component id =1 ( for mission planner )
+    target component id : 1 (= Mission Planner)
     best resource: https://stackoverflow.com/questions/53394660/receiving-and-sending-mavlink-messages-using-pymavlink-library
     https://ardupilot.org/dev/docs/mavlink-basics.html
 '''
-print('start')
+
 # Start a connection listening on a UDP port
-cube_orange_connection = mavutil.mavlink_connection('udpin:localhost:0002') # returns mavutil.mavudp object
+# cube_orange_connection = mavutil.mavlink_connection('udpin:localhost:5762') # returns mavutil.mavudp object
+cube_orange_connection = mavutil.mavlink_connection('com6')
 
 # change the flight
 print('connected')
-# Wait for the first heartbeat 
-#   This sets the system and component ID of remote system for the link
+# Wait for the first heartbeat. This sets the system and component ID of remote system for the link.
 cube_orange_connection.wait_heartbeat(timeout=10) # shows that the system is responding
-
+# cube_orange_connection.target_system = 255
+# cube_orange_connection.target_component = 1
 print("Heartbeat from system (system %u component %u)" % (cube_orange_connection.target_system, cube_orange_connection.target_component))
-print(type(cube_orange_connection))
-
+# MAVLink_sys_status_message()
+cube_orange_connection.mav.heartbeat_send(type=mavutil.mavlink.MAV_TYPE_GENERIC,#MAV_TYPE_GENERIC,
+                                          autopilot=1,
+                                          base_mode=0,
+                                          custom_mode=0,
+                                          system_status=0) # autopilot=1 is pixhawk, 
 # Once connected, use 'the_connection' to get and send messages
-cube_orange_connection.mav.send('this is a test')
-# cube_orange_connection.mav.
-# cube_orange_connection.mav.write()
+# cube_orange_connection.mav.send('this is a test')
+if cube_orange_connection.mode_mapping():
+    print(cube_orange_connection.mode_mapping()['STABILIZE'])
+cube_orange_connection.mav.set_mode_send(
+    cube_orange_connection.target_system,
+    mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+    1)
 cube_orange_connection.close()
-
-
