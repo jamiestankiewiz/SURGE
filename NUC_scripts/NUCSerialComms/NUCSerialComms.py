833ec265dce7c5e687618d1459846b396ca403e4
andreasbrecl <andreas@brecl.com>
2022-10-25 22:47:39 -0600

    Wrote some disk usage and serial communication lib

--- NUC_scripts/NUCSerialComms/NUCSerialComms.py
+++ NUC_scripts/NUCSerialComms/NUCSerialComms.py
@@ -1,38 +1,80 @@
 """
-add comments
+Author: Andreas Brecl
+Date: 10/25/2022
 
+This class deals with sending the serial communication to the cube orange
+from the NUC. This will determine if the communication message needs to be
+updated saying that the system is writing if it is not writing the data
+being collected.
 """
 
 # Import mavutil
-import time
 from pymavlink import mavutil
+from DiskUsageMonitor import DiskUsageMonitor
 
 class NUCSerialComms:
-    def __init__(self, serialPort, serialSpeed):
+    def __init__(self, serialPort, serialSpeed, pathToDisk):
         """
-        
+        This initialized the variable names and the object for pymavlink.
+
+        Input:  serialPort <int> - Port that cube orange will connect to
+                serialSPeed <int> - Serial rate to write data to cube oranage
+
+        Output: None
         """
-    
+        # Define variables
         self.serialPort = serialPort
         self.serialSpeed = serialSpeed
+        self.pathToDisk = pathToDisk
 
-    def sendMessageToCube(self):
-        """
-        
-        """
-        # Establish a connection between the NUC and Cubeorange
-        self.initialize(self)
-
-        # Determine if payload is operating correctly
-        
-
-
-    def initialize(self):
-        """
-        
-        """
         # Create cube orange connection object
-        cube_orange_connection = mavutil.mavlink_connection(self.serialPort, baud=self.serialSpeed)
+        self.cubeOrangeConnection = mavutil.mavlink_connection(self.serialPort, baud=self.serialSpeed)
 
         # Establish that a connection has occured
-        cube_orange_connection.wait_heartbeat()
+        self.cubeOrangeConnection.wait_heartbeat()
+
+        # Create disk usagage object
+        self.diskUsage = DiskUsageMonitor()
+
+    def determineMessageForCube(self):
+        """
+        This function will pull from the disk drive write check function
+        and then determine what kind of message needs to be sent to the
+        cube orange via MAVLink.
+
+        Input:  None
+
+        Output: None
+        """
+        # See if disk is writting data
+        diskStatus = self.diskUsage.isWritingToDisk(self.pathToDisk)
+
+        # Determine if payload is operating correctly NEEDS UPDATING
+        if diskStatus == True:
+
+            # Define messages
+            messageType = mavutil.mavlink.MAV_SEVERITY_ALERT
+            messageText = 'Disk Writing'
+
+            # Send message to cube
+            self.sendMessageToCube(messageType, messageText)
+
+        else:
+            # Define messages
+            messageType = mavutil.mavlink.MAV_SEVERITY_EMERGENCY
+            messageText = 'Disk NOT Writing'
+
+            # Send message to cube
+            self.sendMessageToCube(messageType, messageText)
+
+    def sendMessageToCube(self, messageType, messageText):
+        """
+        This function sends the mavlink message to the cube orange.
+
+        Input:  messageType <object> - mavlink message type
+                messageText <string> - string to be sent with mavlink message
+
+        Output: None
+        """
+        # Send serial communication to NUC
+        self.cubeOrangeConnection.mav.statustext_send(messageType, messageText)
--- NUC_scripts/main/main.py
+++ NUC_scripts/main/main.py
@@ -5,13 +5,10 @@ Author: Andreas Brecl
 Date: 10/25/2022
 
 This script will run the main functionality of the NUC.
-
 """
 
 # Import libraries
 from NUCSerialComms import NUCSerialComms
-
-
 
 def executeFunctions():
     """
@@ -25,16 +22,22 @@ def executeFunctions():
     # Define initial variables
     serialSpeed = 921600
     serialPort = "/dev/ttyUSB0"
+    pathToDisk = "/media/DataStore/"
 
     # Create objects
-    serialComms = NUCSerialComms(serialPort, serialSpeed)
+    serialComms = NUCSerialComms(serialPort, serialSpeed, pathToDisk)
 
     # Start fuctions
-    
+    serialComms.determineMessageForCube()
 
 def main():
     """
+    This excutes the primarity defintions of the main 
+    script.
     
+    Input:  None
+
+    Output: None
     """
     executeFunctions()
 
