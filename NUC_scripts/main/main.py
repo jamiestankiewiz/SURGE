f17a5d5c2866df6c0804a7b472625ad8fbc01593
andreasbrecl <andreas@brecl.com>
2022-10-25 23:17:46 -0600

    file name changes

--- NUC_scripts/main/main.py
+++ NUC_scripts/main/main.py
@@ -8,7 +8,7 @@ This script will run the main functionality of the NUC.
 """
 
 # Import libraries
-from nuc_scripts.main.lib.nuc_serial_comms import NUCSerialComms
+from NUC_scripts.main.lib.nuc_serial_comms import NUCSerialComms
 
 def executeFunctions():
     """
