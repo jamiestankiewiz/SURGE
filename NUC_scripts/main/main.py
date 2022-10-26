175f9b351d035bc89f56bef1b5cc54b07c7fa17d
andreasbrecl <andreas@brecl.com>
2022-10-26 00:05:31 -0600

    updated lib paths

--- NUC_scripts/main/main.py
+++ NUC_scripts/main/main.py
@@ -8,7 +8,7 @@ This script will run the main functionality of the NUC.
 """
 
 # Import libraries
-from .nuc_serial_comms import NUCSerialComms
+from lib.nuc_serial_comms import NUCSerialComms
 
 def executeFunctions():
     """
