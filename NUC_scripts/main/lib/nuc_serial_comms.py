ed3e2b9ee196c5116ecb3cbb0a57e637acd89e41
andreasbrecl <andreas@brecl.com>
2022-10-25 23:14:13 -0600

    updated file structure

--- NUC_scripts/main/lib/nuc_serial_comms.py
+++ NUC_scripts/main/lib/nuc_serial_comms.py
@@ -10,7 +10,7 @@ being collected.
 
 # Import mavutil
 from pymavlink import mavutil
-from DiskUsageMonitor import *
+from nuc_scripts.main.lib.disk_usage_monitor import DiskUsageMonitor
 
 class NUCSerialComms:
     def __init__(self, serialPort, serialSpeed, pathToDisk):
--- NUC_scripts/main/main.py
+++ NUC_scripts/main/main.py
@@ -8,7 +8,7 @@ This script will run the main functionality of the NUC.
 """
 
 # Import libraries
-from NUCSerialComms import *
+from nuc_scripts.main.lib.nuc_serial_comms import NUCSerialComms
 
 def executeFunctions():
     """
