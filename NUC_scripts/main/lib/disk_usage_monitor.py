b5d587f936cda44bc9081459f9028cb394874793
andreasbrecl <andreas@brecl.com>
2022-10-27 13:20:41 -0600

    updated all changes

--- NUC_scripts/main/lib/disk_usage_monitor.py
+++ NUC_scripts/main/lib/disk_usage_monitor.py
@@ -11,61 +11,61 @@ import psutil
 import time
 
 class DiskUsageMonitor:
-        def __init__(self, pathToDisk):
-            """
-            This initialized the variable names and the object for the 
-            disk monitoring processes.
+    def __init__(self, pathToDisk):
+        """
+        This initialized the variable names and the object for the 
+        disk monitoring processes.
 
-            Input:  pathToDisk <string> - Path to harddrive
+        Input:  pathToDisk <string> - Path to harddrive
 
-            Output: None
-            """
-            self.pathToDisk = pathToDisk
+        Output: None
+        """
+        self.pathToDisk = pathToDisk
 
-        def isDiskWriting(self):
-            """
-            This function will pull the disk data then pause momentairly
-            before checking if the disk has pulled data again. This will
-            allow for the data usage to update and see if it has changed.
+    def isDiskWriting(self):
+        """
+        This function will pull the disk data then pause momentairly
+        before checking if the disk has pulled data again. This will
+        allow for the data usage to update and see if it has changed.
 
-            Input:  None
+        Input:  None
 
-            Output: None
-            """
-            # Pull disk information
-            spaceUsedFirstPull = self.pullDiskData()
+        Output: None
+        """
+        # Pull disk information
+        spaceUsedFirstPull = self.pullDiskData()
 
-            # Sleep for half a second
-            time.sleep(0.5)
+        # Sleep for half a second
+        time.sleep(0.5)
 
-            # Pull disk information
-            spaceUsedSecondPull = self.pullDiskData()
+        # Pull disk information
+        spaceUsedSecondPull = self.pullDiskData()
 
-            # Check if data was written
-            if spaceUsedSecondPull > spaceUsedFirstPull:
-                diskStatus = True
-            else:
-                diskStatus = False
+        # Check if data was written
+        if spaceUsedSecondPull > spaceUsedFirstPull:
+            diskStatus = True
+        else:
+            diskStatus = False
 
-            # Return status
-            return diskStatus
+        # Return status
+        return diskStatus
 
 
-        def pullDiskData(self):
-            """
-            This function pulls the space used on a harddrive.
+    def pullDiskData(self):
+        """
+        This function pulls the space used on a harddrive.
 
-            Input:  None
+        Input:  None
 
-            Output: None
-            """
-            # Pull disk information
-            diskInformation = psutil.disk_usage(self.pathToDisk)
+        Output: None
+        """
+        # Pull disk information
+        diskInformation = psutil.disk_usage(self.pathToDisk)
 
-            # Seperate variables
-            spaceUsed = diskInformation[1]
+        # Seperate variables
+        spaceUsed = diskInformation[1]
 
-            # Return space used
-            return spaceUsed
+        # Return space used
+        return spaceUsed
 
 
