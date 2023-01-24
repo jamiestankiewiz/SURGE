7caded227f5d1a439194798c6085dbbd1aa6a6cb
andreasbrecl <andreas@brecl.com>
2023-01-23 23:23:44 -0700

    changed main script & added unit testing

--- NUC_scripts/main/tests/NUC_connection_test.py
+++ NUC_scripts/main/tests/NUC_connection_test.py
@@ -1,22 +1,14 @@
-import random
-import time
-from pymavlink import mavutil
+"""
+Auhtor: Andreas Brecl
+Date: 01/23/2023
 
-# Start a connection listening on a UDP port
-# cube_orange_connection = mavutil.mavlink_connection('udpin:localhost:5762') # returns mavutil.mavudp object
-cube_orange_connection = mavutil.mavlink_connection("/dev/ttyUSB0", baud=921600)
+This script will test the functionality of the 
+"""
 
-# Wait for the first heartbeat. This sets the system and component ID of remote system for the link.
-cube_orange_connection.wait_heartbeat() # shows that the system is responding
-print("Connected!")
 
-while True:
-    num = round(random.random())
-    if num == 1:
-            numString = str(numaster.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_ALERT,numString.encode())
-    else:
-            numString = str(num)
-            master.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_EMERGENCY,numString.encode())
-    time.sleep(1)
+# Import pytest
+import pytest
 
-cube_orange_connection.close()
+
+if __name__ == "__main__":
+    main()
