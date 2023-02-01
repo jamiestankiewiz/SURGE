0b9de83282b57a8413648456155cb06abe3da0c8
andreasbrecl <andreas@brecl.com>
2023-02-01 11:10:25 -0700

    added PKK scripts

---
+++ PPK_scripts/main.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+"""
+Author: Andreas Brecl
+Date: 02/01/2023
+
+This script will run the PPK to save software to the NUC.
+"""
+
+# Import libraries
+import time
+import os
+import serial
+
+def main():
+    """
+    This will run the main functionality of the PPK to NUC
+    file saving.
+
+    Input:  None
+
+    Output: None
+    """
+    # Define base variables
+    pathToDisk = "/media/PPK_data"
+
+    # Get file name
+    newFileName = checkForFileNumber(pathToDisk)
+
+    # Create serial object
+    ser = serial.Serial(
+        port='/dev/serial0',
+        baudrate=38400,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+    )
+
+    # Save information to file
+    while True:
+
+        # Read in data
+        data = str(ser.readline())
+
+        # Save data
+        f = open(newFileName, "a")
+        f.writelines(data)
+        f.close()
+
+
+def checkForFileNumber(pathToDisk):
+    """
+    This function will check for the current file number that
+    has been written the output the number that the new file
+    needs.
+
+    Input:  pathToDisk <str> - Path to save location
+
+    Output: newFileName <str> - New file name to write to
+    """
+    # Define checker vars
+    fileNumber = 0
+
+    # Pull list of files in directory
+    directoryList = os.listdir(pathToDisk)
+
+    # Iterate through files
+    if directoryList != None:
+        for file in directoryList:
+            splitFile = file.split("_")
+            fileNumberCurrent = splitFile[0][0]
+
+            # See if number is larger
+            if fileNumberCurrent > fileNumber:
+                fileNumber = fileNumberCurrent
+
+        # Create new file name
+        newFileNumber = fileNumber + 1
+        newFileName = "PPKdata_" + str(newFileNumber) + ".txt"    
+
+    else:
+        newFileName = "PPKdata_1.txt"
+
+    return newFileName
+
+if __name__ == "__main__":
+    main()
